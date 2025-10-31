e07a192cf6bea1ee0bd93d7fa240d102ef49394b
Jeremy <235932977+jeremyctrl@users.noreply.github.com>
2025-10-31 17:24:44 +0200

    feat(googlesearch): implement basic operators

--- internal/googlesearch/googlesearch.go
+++ internal/googlesearch/googlesearch.go
@@ -1,6 +1,9 @@
 package googlesearch
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type GoogleSearch struct {
 	tags []string
@@ -12,4 +15,19 @@ func New() *GoogleSearch {
 
 func (g *GoogleSearch) String() string {
 	return strings.Join(g.tags, " ")
+}
+
+func (g *GoogleSearch) Or() *GoogleSearch {
+	g.tags = append(g.tags, "|")
+	return g
+}
+
+func (g *GoogleSearch) And() *GoogleSearch {
+	g.tags = append(g.tags, "&")
+	return g
+}
+
+func (g *GoogleSearch) Group(q *GoogleSearch) *GoogleSearch {
+	g.tags = append(g.tags, fmt.Sprintf("(%s)", q.String()))
+	return g
 }
