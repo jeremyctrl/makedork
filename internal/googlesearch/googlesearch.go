d2e3a0903ee347f88d164e87172ca4fff447533b
Jeremy <235932977+jeremyctrl@users.noreply.github.com>
2025-10-31 17:37:24 +0200

    feat(googlesearch): implement filters starting with A

--- internal/googlesearch/googlesearch.go
+++ internal/googlesearch/googlesearch.go
@@ -2,6 +2,7 @@ package googlesearch
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -11,6 +12,14 @@ type GoogleSearch struct {
 
 func New() *GoogleSearch {
 	return &GoogleSearch{}
+}
+
+func joinTag(tag, value string, quotes bool) string {
+	if quotes {
+		return tag + strconv.Quote(value)
+	}
+
+	return tag + value
 }
 
 func (g *GoogleSearch) String() string {
@@ -29,5 +38,31 @@ func (g *GoogleSearch) And() *GoogleSearch {
 
 func (g *GoogleSearch) Group(q *GoogleSearch) *GoogleSearch {
 	g.tags = append(g.tags, fmt.Sprintf("(%s)", q.String()))
+	return g
+}
+
+// A
+func (g *GoogleSearch) AllInAnchor(value string) *GoogleSearch {
+	g.tags = append(g.tags, joinTag("allinanchor:", value, true))
+	return g
+}
+func (g *GoogleSearch) AllInPostAuthor(value string) *GoogleSearch {
+	g.tags = append(g.tags, joinTag("allinpostauthor:", value, true))
+	return g
+}
+func (g *GoogleSearch) AllInText(value string) *GoogleSearch {
+	g.tags = append(g.tags, joinTag("allintext:", value, true))
+	return g
+}
+func (g *GoogleSearch) AllInTitle(value string) *GoogleSearch {
+	g.tags = append(g.tags, joinTag("allintitle:", value, true))
+	return g
+}
+func (g *GoogleSearch) AllInURL(value string) *GoogleSearch {
+	g.tags = append(g.tags, joinTag("allinurl:", value, true))
+	return g
+}
+func (g *GoogleSearch) After(date string) *GoogleSearch {
+	g.tags = append(g.tags, joinTag("after:", date, false))
 	return g
 }
